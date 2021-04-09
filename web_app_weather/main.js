2e48be430f64ee550ab73342d7f64b0756890854
duonghungit24 <hungduonng721@gmail.com>
2021-04-09 23:49:58 +0700

    web_weather

---
+++ web_app_weather/main.js
@@ -0,0 +1,31 @@
+const api = '0862d0977a83fb32c1f12db38ff37a6e';
+const search = document.querySelector('.search');
+const cityName = document.querySelector('.city-name');
+const wetherstatus = document.querySelector('.wether-status');
+const temperature = document.querySelector('.temperature');
+const weatherIcon = document.querySelector('.wethear-icon');
+const sunrise = document.querySelector('.start');
+const sunset = document.querySelector('.end');
+const humidity = document.querySelector('.humidity');
+const wind = document.querySelector('.wind');
+
+
+
+
+search.addEventListener('change',(e) => {
+    fetch(`http://api.openweathermap.org/data/2.5/weather?q=${e.target.value}&lang=vi&appid=${api}&units=metric`)
+        .then(response => {
+            return response.json();
+        })
+        .then(data => {
+            console.log(data)
+            cityName.innerHTML = data.name || '---';
+            wetherstatus.innerHTML = data.weather[0].description;
+            weatherIcon.setAttribute('src' ,`http://openweathermap.org/img/wn/${data.weather[0].icon}.png`);
+            temperature.innerHTML = Math.round(data.main.temp);
+            sunrise.innerHTML = moment.unix(data.sys.sunrise).format('H:mm');
+            sunset.innerHTML = moment.unix(data.sys.sunset).format('H:mm');
+            humidity.innerHTML = data.main.humidity;
+            wind.innerHTML = data.wind.speed;
+        })
+})
